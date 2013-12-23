87d00c0eac76fcec3e39c9455e765170321ee480
Arto Bendiken <arto@bendiken.net>
2013-12-23 22:41:04 +0100

    Implemented the PARSE-DECIMAL function.

---
+++ common-lisp/parse-decimal.lisp
@@ -0,0 +1,13 @@
+;; This is free and unencumbered software released into the public domain.
+
+(defun parse-decimal (input-string)
+  "Parses a decimal string, returning the corresponding rational number."
+  (declare (type string input-string))
+  (let* ((separator-position (or (position #\. input-string) 0))
+         (integer-part       (parse-integer input-string :junk-allowed t
+                                            :end separator-position))
+         (fractional-part    (parse-integer input-string :junk-allowed t
+                                            :start (1+ separator-position)))
+         (fractional-length  (- (length input-string) (1+ separator-position))))
+    (when (and integer-part fractional-part)
+      (+ integer-part (/ fractional-part (expt 10 fractional-length))))))
