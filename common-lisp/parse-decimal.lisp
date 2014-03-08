4ce3a493fc0e3961f89ab70e568c78008f2edad3
Arto Bendiken <arto@bendiken.net>
2014-03-08 22:28:00 +0100

    Corrected the file header format.

--- common-lisp/parse-decimal.lisp
+++ common-lisp/parse-decimal.lisp
@@ -1,4 +1,4 @@
-;; This is free and unencumbered software released into the public domain.
+;;;; This is free and unencumbered software released into the public domain.
 
 (defun parse-decimal (input-string)
   "Parses a decimal string, returning the corresponding rational number."
