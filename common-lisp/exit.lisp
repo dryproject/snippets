96ec16728ded8a982677fa734aa6167dc8845ddd
Arto Bendiken <arto@bendiken.net>
2014-03-09 05:04:59 +0100

    Implemented a portable EXIT function for Common Lisp.
    This has been correctly conditionalized for and validated with
    all current open-source Common Lisp implementations of note.

---
+++ common-lisp/exit.lisp
@@ -0,0 +1,15 @@
+;;;; This is free and unencumbered software released into the public domain.
+
+(defun exit (&optional (code 0))
+  "Terminates the process using the exit code CODE."
+  (declare (type (signed-byte 8) code))
+  #+abcl (ext:quit :status code)
+  #+ccl (ccl:quit code)
+  #+clisp (ext:quit code)
+  #+cmu (unix:unix-exit code)
+  #+ecl (si:quit code)
+  #+gcl (lisp:quit code)
+  #+mkcl (mk-ext:quit :exit-code code)
+  #+sbcl (sb-ext:exit :code code)
+  #+xcl (ext:quit :status code)
+  (error "process terminated with exit code ~S" code))
