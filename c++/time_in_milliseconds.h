fbfa3a1a72f69c1275acda81cb30a9980904ae12
Arto Bendiken <arto@bendiken.net>
2014-03-29 14:36:13 +0100

    Implemented a time_in_milliseconds() function for C++11.

--- c++/time_in_milliseconds.h
+++ c++/time_in_milliseconds.h
@@ -1,25 +1,25 @@
 /* This is free and unencumbered software released into the public domain. */
 
-#ifndef TIME_IN_MICROSECONDS_H
-#define TIME_IN_MICROSECONDS_H
+#ifndef TIME_IN_MILLISECONDS_H
+#define TIME_IN_MILLISECONDS_H
 
 #include <chrono>  /* for std::chrono */
 #include <cstdint> /* for std::uint64_t */
 
 /**
- * Returns the current time as the microseconds since the epoch.
+ * Returns the current time as the milliseconds since the epoch.
  *
  * This function requires at minimum the C++11 standard library.
  *
- * @return the number of microseconds since the epoch
+ * @return the number of milliseconds since the epoch
  * @see http://en.cppreference.com/w/cpp/chrono/steady_clock
  */
 static inline std::uint64_t
-time_in_microseconds() {
+time_in_milliseconds() {
   using clock = std::chrono::steady_clock; /* a monotonic clock */
-  using precision = std::chrono::microseconds;
+  using precision = std::chrono::milliseconds;
   using std::chrono::duration_cast;
   return duration_cast<precision>(clock::now().time_since_epoch()).count();
 }
 
-#endif /* TIME_IN_MICROSECONDS_H */
+#endif /* TIME_IN_MILLISECONDS_H */
